57d85ea5a22bcd9aaccd868eae9716865c3750ce
fleckb <bernhard.fleck@erstegroup.com>
2020-03-27 14:44:11 +0100

    Add comment which describes next steps & design outline

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,10 @@
 fun main(vararg args: String) {
+    /*
+    - Read content of args[0] into list of strings
+    - Create table
+    - Print first page of table
 
+    Content -> CsvParser -> Table -> TableView (pagination) -> ConsoleRenderer
+    UserInterface (Table, UserInput -> TableView -> ConsoleRenderer)
+    */
 }
